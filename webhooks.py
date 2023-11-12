f7a26c03e8be018848ab0c075a70b20d2b7f7372
soseonghyeon <thtjdgus65@gmail.com>
2023-11-12 13:46:49 +0900

    buildBranch, buildPath 수정

--- webhooks.py
+++ webhooks.py
@@ -5,8 +5,8 @@ from flask_restful import Resource, Api
 
 import os
 
-buildBranch = 'master'
-buildPath = '/home/ubuntu/lab-socket-programming'
+buildBranch = 'main'
+buildPath = '/home/thtjdgus65/lab-socket-programming'
 
 buildCommand = 'cd ' + buildPath + ' && git stash && git pull origin ' + buildBranch
 
